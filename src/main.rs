e093a22dee798b9b72749a1c289f5f681a96772a
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2023-07-25 15:32:47 -0400

    Suprise, it works

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::net::{Ipv4Addr, SocketAddr, IpAddr};
+use actix_web::{get, post, web::{self, Form}, App, HttpResponse, HttpServer, Responder, HttpRequest};
 use askama::Template; // bring trait in scope
 use cmanager::{self, Comment};
+use serde::Deserialize;
 
 
 #[derive(Template)] // this will generate the code...
@@ -9,11 +11,29 @@ pub struct CommentTemplate {
     comments: Vec<cmanager::Comment>,
 }
 
+#[derive(Deserialize)]
+#[derive(Debug)]
+struct FormData {
+    username: String,
+    comment: String,
+}
+
+async fn new(web::Form(form): web::Form<FormData>, req: HttpRequest) -> CommentTemplate {
+    let ip = req.peer_addr().unwrap_or(SocketAddr::new(IpAddr::V4(Ipv4Addr::new(6, 6, 6, 6)), 666));
+    let username = form.username;
+    let comment = form.comment;
+
+    let _ = cmanager::new(ip.to_string(), username, comment);
+
+    CommentTemplate {comments: cmanager::get_all()}
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(web::resource("/").to(|| async { CommentTemplate {comments: cmanager::get_all()}}))
+            .route("/new", web::post().to(new))
     })
     .bind(("127.0.0.1", 8080))?
     .run()

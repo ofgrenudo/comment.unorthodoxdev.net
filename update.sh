341a676dcfbff8fb0ad6ca5e798e2b1047dba6c8
Joshua Winters-Brown <joshuawintersbrown@gmail.com>
2023-11-28 10:37:41 -0500

    Removed any commited db's

--- update.sh
+++ update.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-@echo off
-echo Backing up the current running comments.db
+printf "=======================================================\n"
+printf "||                                                   ||\n"
+printf "||                                                   ||\n"
+printf "||do not make commits on prod or else you will break ||\n"
+printf "||      the damn database. Remember 11.28.2023       ||\n"
+printf "||                                                   ||\n"
+printf "||                                                   ||\n"
+printf "=======================================================\n\n"
+printf "Backing up the comments database to \`backups\\\` \n\n"
+
 cp comments.db backup/comments_`date +"%Y%m%d_%H%M"`.db  
+printf "comments.db has been susscessfully backed up.\n"
+

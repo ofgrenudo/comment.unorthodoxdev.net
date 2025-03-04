49dd4419075531fe8b1a0451d35c92bbd8c95c6a
Joshua <joshuawintersbrown@gmail.com>
2025-03-04 09:56:19 -0500

    added new comment_on_post that does a different behavior than comment does

--- src/new.rs
+++ src/new.rs
@@ -1,6 +1,7 @@
-use actix_web::{HttpRequest, web, http::header::HeaderValue, Responder, route};
+use actix_web::{http::{header::{ContentType, HeaderValue}, StatusCode}, route, web, HttpRequest, HttpResponse, Responder};
 use log::debug;
 use serde::Deserialize;
+use serde_json::Value;
 
 #[derive(Deserialize)]
 #[derive(Debug)]
@@ -28,7 +29,34 @@ pub async fn comment(web::Form(form): web::Form<FormData>, req: HttpRequest) -> 
 }
 
 #[route("/comment/new/post", method="POST", method="PUT")]
-pub async fn comment_on_post(web::Form(form): web::Form<FormData>, req: HttpRequest) -> impl Responder {
+pub async fn comment_on_post(web::Form(form): web::Form<FormData>, req: HttpRequest) -> impl Responder  {
+    debug!("{:#?}", form);
+    let default_ip = HeaderValue::from_static("127.0.0.1");
+    let ip_ref = req.headers().get("X-Forwarded-For").unwrap_or(&default_ip);
+    let ip = ip_ref.to_str().unwrap_or("6.6.6.6");
+    
+    let username = form.username;
+    let comment_text = form.comment;  // Renamed from `comment`
+    let post_url = form.post_url;
+
+    debug!(
+        "Received create new comment request for a post with the URL: {}, username: {}, and comment: {}",
+        post_url, username, comment_text
+    );
+
+    let new_comment_result = cmanager::new::comment_on_post(String::from(ip), username, comment_text, post_url.clone());
+    debug!("Submitted new comment {:?}", new_comment_result);
+    debug!("Redirecting to the root /comment/get/latest");
+    // Return an acknowledgment response
+    HttpResponse::Ok()
+        .content_type(ContentType::plaintext())
+        .insert_header(("X-Hdr", "sample"))
+        .body("data")
+}
+
+#[route("/comment/debug", method="POST")]
+pub async fn post_any_data(web::Form(form): web::Form<FormData>, req: HttpRequest) -> impl Responder {
+    debug!("{:#?}", form);
     let default_ip = HeaderValue::from_static("127.0.0.1");
     let ip_ref = req.headers().get("X-Forwarded-For").unwrap_or(&default_ip);
     let ip = ip_ref.to_str().unwrap_or("6.6.6.6");

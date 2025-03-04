3e068b07b39f3fcfd63f6e979a508c92eb3247d5
Joshua <joshuawintersbrown@gmail.com>
2025-03-04 09:55:59 -0500

    added cors and post route

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::path::Path;
 
 use actix_web::{App, HttpServer};
+use actix_cors::Cors;
 use log4rs;
 use log::*;
 
@@ -11,20 +12,33 @@ mod get;
 async fn main() -> std::io::Result<()> {
     log4rs::init_file("log_config.yaml", Default::default()).unwrap();
 
+
+
     info!("running now on https://0.0.0.0:8080/comment/");
     HttpServer::new(|| {
+        let cors = Cors::default()
+            .allowed_origin("http://localhost:1313")
+            .allowed_origin("https://unorthodoxdev.net/")
+            .allowed_methods(vec!["GET", "POST", "PUT"])
+            .allowed_headers(vec![actix_web::http::header::CONTENT_TYPE])
+            .max_age(3600);
+
         App::new()
-        .service(get::default)
-        .service(get::latest)
-        .service(get::all)
-        .service(get::by_id)
-        .service(get::by_username)
-        .service(new::comment)
-        .service(new::comment_on_post)
-        .service(actix_files::Files::new(
-            "/comment/static/",
-            Path::new(&format!("./static")),
-        ))
+            .wrap(cors)
+            .service(get::default)
+            .service(get::latest)
+            .service(get::all)
+            .service(get::by_id)
+            .service(get::by_username)
+            .service(get::by_post)
+            .service(get::by_post_raw)
+            .service(new::comment)
+            .service(new::comment_on_post)
+            .service(new::post_any_data)
+            .service(actix_files::Files::new(
+                "/comment/static/",
+                Path::new(&format!("./static")),
+            ))
     })
     .bind(("0.0.0.0", 8080))?
     .run()
